77b889476c8f2196c95ed36b538b649dc08bc841
Kieran <kieransweeden@gmail.com>
2023-04-02 11:43:15 +0100

    Add create post wizard view

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,16 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+        port: "",
+        pathname: "/oauth_github/**",
+      },
+    ],
+  },
 
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
